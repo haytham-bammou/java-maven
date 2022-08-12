46a86b44fbabe39061b16d59e7cf89191c169c72
haytham-bammou <Haythambm86@gmail.com>
2022-08-12 17:41:02 +0100

    multibransh pipeline

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t haythambammou/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push haythambammou/my-repo:jma-2.0'
     }
 } 
 
